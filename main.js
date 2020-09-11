55adab38b50655a3f73e2309a26ef86147761511
Kazdan1994 <arthurjacquemin2@gmail.com>
2020-09-11 17:30:56 +0200

    remove stick

--- main.js
+++ main.js
@@ -7,3 +7,17 @@ for (let i = 0; i < 10; i++) {
 document
     .querySelector('#app')
     .innerHTML = map.join(' ')
+
+const value = document
+    .querySelector('#nb')
+    .value
+
+const button = document
+    .querySelector('button')
+    .addEventListener('click', function () {
+        map.splice(0, 3)
+
+        document
+            .querySelector('#app')
+            .innerHTML = map.join(' ')
+    })
